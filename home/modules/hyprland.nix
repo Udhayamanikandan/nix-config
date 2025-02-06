66f16c92a8f44d3d8b41ace3621ed205ca6c84d9
Narendra S <narendra.s1232@gmail.com>
2025-02-06 20:13:22 +0530

    fullscreen hyprland and  fix hyprlock clock

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -3,7 +3,7 @@
   lib,
   ...
 }: let
-  wallpaper = ./../../files/wallpapers/wallpaper.jpg;
+  wallpaper = ./../../files/wallpapers/nix-logo.png;
   hyprland_config = ./../../files/configs/hypr;
 in {
   imports = [
@@ -52,7 +52,7 @@ in {
       background {
           monitor =
           path = ${wallpaper}
-          blur_passes = 3
+          blur_passes = 1
           contrast = 0.8916
           brightness = 0.8172
           vibrancy = 0.1696
@@ -65,70 +65,12 @@ in {
           disable_loading_bar = true
       }
 
-      # DP-1 Conifg
-      input-field {
-          monitor = DP-1
-          size = 250, 60
-          outline_thickness = 2
-          dots_size = 0.2 # Scale of input-field height, 0.2 - 0.8
-          dots_spacing = 0.2 # Scale of dots' absolute size, 0.0 - 1.0
-          dots_center = true
-          outer_color = rgba(0, 0, 0, 0)
-          inner_color = rgba(0, 0, 0, 0.5)
-          font_color = rgb(200, 200, 200)
-          fade_on_empty = false
-          capslock_color = -1
-          placeholder_text = <i><span foreground="##e6e9ef">Password</span></i>
-          fail_text = <i>$FAIL <b>($ATTEMPTS)</b></i>
-          hide_input = false
-          position = 0, -120
-          halign = center
-          valign = center
-      }
-
-      # Date
-      label {
-        monitor = DP-1
-        text = cmd[update:1000] echo "<span>$(date '+%A, %d %B')</span>"
-        color = rgba(255, 255, 255, 0.8)
-        font_size = 15
-        font_family = JetBrains Mono Nerd Font Mono ExtraBold
-        position = 0, -400
-        halign = center
-        valign = top
-      }
-
-      # Time
-      label {
-          monitor = DP-1
-          text = cmd[update:1000] echo "<span>$(date '+%H:%M')</span>"
-          color = rgba(255, 255, 255, 0.8)
-          font_size = 120
-          font_family = JetBrains Mono Nerd Font Mono ExtraBold
-          position = 0, -400
-          halign = center
-          valign = top
-      }
-
-      # Keyboard layout
-      label {
-        monitor = DP-1
-        text = $LAYOUT
-        color = rgba(255, 255, 255, 0.9)
-        font_size = 10
-        font_family = JetBrains Mono Nerd Font Mono
-        position = 0, -175
-        halign = center
-        valign = center
-      }
-
       # eDP-1 Conifg
       input-field {
           monitor = eDP-1
-          size = 500, 120
+          size = 250, 60
           outline_thickness = 2
-          dots_size = 0.2 # Scale of input-field height, 0.2 - 0.8
-          dots_spacing = 0.2 # Scale of dots' absolute size, 0.0 - 1.0
+          dots_size = 0.2
           dots_center = true
           outer_color = rgba(0, 0, 0, 0)
           inner_color = rgba(0, 0, 0, 0.5)
@@ -150,33 +92,20 @@ in {
         color = rgba(255, 255, 255, 0.8)
         font_size = 30
         font_family = JetBrains Mono Nerd Font Mono ExtraBold
-        position = 0, -400
         halign = center
-        valign = top
+        valign = center
       }
 
       # Time
       label {
           monitor = eDP-1
-          text = cmd[update:1000] echo "<span>$(date '+%H:%M')</span>"
+          text = cmd[update:1000] echo "<span>$(date '+%H:%M:%S')</span>"
           color = rgba(255, 255, 255, 0.8)
-          font_size = 240
+          font_size = 70
+          position = 0, 120
           font_family = JetBrains Mono Nerd Font Mono ExtraBold
-          position = 0, -400
           halign = center
-          valign = top
-      }
-
-      # Keyboard layout
-      label {
-        monitor = eDP-1
-        text = $LAYOUT
-        color = rgba(255, 255, 255, 0.9)
-        font_size = 20
-        font_family = JetBrains Mono Nerd Font Mono
-        position = 0, -230
-        halign = center
-        valign = center
+          valign = center
       }
     '';
   };
@@ -217,14 +146,9 @@ in {
       "disable-vpn-notifications" = true;
     };
 
-    "org/gtk/gtk4/settings/file-chooser" = {
-      "show-hidden" = true;
-    };
-
     "org/gtk/settings/file-chooser" = {
       "date-format" = "regular";
       "location-mode" = "path-bar";
-      "show-hidden" = true;
       "show-size-column" = true;
       "show-type-column" = true;
       "sort-column" = "name";
